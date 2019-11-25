d82a91eb18d747bd33f8fba6fee100c9d8cdd2b6
nick33337 <diamond007!>
2019-11-25 21:01:37 +0900

    수정된 코드. 디자이너 사용 안 함 창 사이즈는 (300, 200, 1200, 700)으로 고정했으면 좋겠고 전체 디자인은 구구펀에 있는 인터파크 티켓팅을 참고해서 하면 될 것 같음

--- mainWindow.py
+++ mainWindow.py
@@ -1,18 +1,32 @@
 import sys
 from PyQt5.QtWidgets import *
 
-class EmptyWindow(QMainWindow):
+
+class PushButtonWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setupUI()
+
         self.setWindowTitle("티켓팅 연습 게임")
-        self.setGeometry(300, 200, 1200, 700) # x축, y축, 가로, 세로
+        self.setGeometry(300, 200, 1200, 700)  # x축, y축, 가로, 세로 지정하기
+
+    def setupUI(self):
+        self.setWindowTitle("PushButtonWindow")
+
+        start_btn = QPushButton("시작하기", self)
+        start_btn.move(20, 20)
+        start_btn.setGeometry(400, 600, 150, 50)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = EmptyWindow()
+    window = PushButtonWindow()
     window.show()
     app.exec_()
+
+
 """
+예전 코드!
 class MyDialog(QDialog):
     def __init__(self):
         
