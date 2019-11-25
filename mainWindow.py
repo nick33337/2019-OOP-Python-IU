d400acd507888ad32ba6c2041ee4937ad97390f6
nick33337 <diamond007!>
2019-11-25 20:50:34 +0900

    수정된 코드. 디자이너 사용 안 함

--- mainWindow.py
+++ mainWindow.py
@@ -1,13 +1,37 @@
 import sys
-
 from PyQt5.QtWidgets import *
 
-app = QApplication([])
-dialog = Qdialog([])
-dialog.show()
-app.exec()
+class EmptyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("티켓팅 연습 게임")
+        self.setGeometry(300, 200, 1200, 700) # x축, y축, 가로, 세로
 
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    w = Form()
-    sys.exit(app.exec())
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = EmptyWindow()
+    window.show()
+    app.exec_()
+"""
+class MyDialog(QDialog):
+    def __init__(self):
+        
+        QDialog.__init__(self)
+        # 레이블, 버튼 컨트롤
+        titleName = QLabel("티켓팅 연습 게임")
+        titleName2 = QLabel("제작: 성민경 | 오세현 | 정도영")
+        btnStart = QPushButton("시작하기")
+
+        layout = QVBoxLayout()
+        layout.addWidget(titleName)
+        layout.addWidget(titleName2)
+        layout.addWidget(btnStart)
+
+        self.setLayout(layout)
+        
+
+app = QApplication([])
+dialog = MyDialog([])
+dialog.show()
+app.exec_()
+"""
